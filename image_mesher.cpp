69fe9a7b82aae473a1e907781abd1f278fe5c163
Niko Nevatie <nnevatie@gmail.com>
2015-04-19 18:29:33 +0300

    Add image sampling and block creation.

--- image_mesher.cpp
+++ image_mesher.cpp
@@ -1,5 +1,9 @@
 #include "image_mesher.h"
 
+#include <iostream>
+#include <glm/ext.hpp>
+
+#include "clock.h"
 #include "log.h"
 
 namespace hc
@@ -12,27 +16,59 @@ namespace
 
 struct Block
 {
+    Block()
+    {}
+
+    Block(const glm::vec3& c0, const glm::vec3& c1) : bounds(c0, c1)
+    {}
+
     BoundingBox bounds;
+
+    friend std::ostream& operator<<(std::ostream& out, const Block& block)
+    {
+        out << glm::to_string(block.bounds.first) << ", "
+            << glm::to_string(block.bounds.second);
+
+        return out;
+    }
 };
 
 }
 
 Geometry geometry(const Image& image, float interval)
 {
+    HCTIME(__FUNCTION__);
+
     const Rect<int>             rect   = image.rect();
     const uint8_t* __restrict__ bits   = image.bits();
     const int                   stride = image.stride();
+    const float                depth   = std::min(rect.w, rect.h) / 255.f;
 
-    for (float fy = rect.y; fy < rect.y + rect.h; fy += interval)
+    const int cx = rect.w / interval;
+    const int cy = rect.h / interval;
+
+    std::vector<Block> blocks;
+    blocks.reserve(cx * cy);
+
+    for (int sy = 0; sy < cy; ++sy)
     {
-        const int        y = int(fy + 0.5f);
-        const uint8_t* row = bits + y * stride;
+        const float                  fy = rect.y + sy * interval;
+        const int                     y = int(fy);
+        const uint8_t* __restrict__ row = bits + y * stride;
 
-        for (float fx = rect.x; fx < rect.x + rect.w; fx += interval)
+        for (int sx = 0; sx < cx; ++sx)
         {
-            const int     x = int(fx + 0.5f);
-            const uint8_t p = row[x];
-            HCLOG(Info) << x << ", " << y << ": " << int(p);
+            const int fx = int(rect.x + sx * interval);
+            const int x  = int(fx);
+            const int p  = row[x];
+
+            const glm::vec3 c0(fx, fy, 0.f);
+            const glm::vec3 c1(fx + interval, fy + interval, p * depth);
+
+            const Block block(c0, c1);
+            blocks.push_back(block);
+
+            HCLOG(Info) << x << ", " << y << ": " << int(p) << ", " << block;
         }
     }
 
