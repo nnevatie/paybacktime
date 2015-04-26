ed717c7713f2b90857639b9db1425dd249c0a115
Niko Nevatie <nnevatie@gmail.com>
2015-04-26 17:22:34 +0300

    Extend GL uniforms API.

--- gl_uniforms.h
+++ gl_uniforms.h
@@ -10,6 +10,15 @@ namespace gl
 {
 
 template<>
+ShaderProgram& ShaderProgram::setUniform<glm::vec2>(
+    const char* name, const glm::vec2& v)
+{
+    glUniform2fv(glGetUniformLocation(id, name),
+                 1, glm::value_ptr(v));
+    return *this;
+}
+
+template<>
 ShaderProgram& ShaderProgram::setUniform<glm::vec4>(
     const char* name, const glm::vec4& v)
 {
