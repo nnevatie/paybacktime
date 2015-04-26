324a2a3e89973acd1ddc98e772043a4df11bbec6
Niko Nevatie <nnevatie@gmail.com>
2015-04-26 17:22:21 +0300

    Render wireframe in fragment shader.

--- data/screenspace.fs
+++ data/screenspace.fs
@@ -2,12 +2,17 @@
 
 uniform vec4 in_color;
 
+in  vec3 dist;
 out vec4 out_color;
 
 void main()
 {
-    vec3 n    = normalize(vec3(dFdx(gl_FragCoord.w), dFdy(gl_FragCoord.w), 1));
-    vec3 l    = normalize(vec3(1, 1, 0));
-    float a   = 0.5;
-    out_color = in_color + 50 * max(0.0, dot(n, l));
+    vec3 n     = normalize(vec3(dFdx(gl_FragCoord.w), dFdy(gl_FragCoord.w), 1));
+    vec3 l     = normalize(vec3(1, 10, 0));
+
+    float d    = min(min(dist[0], dist[1]), dist[2]);
+    float edge = exp2(-0.25 * d * d);
+
+    vec4 color = vec4(in_color.xyz + 1000 * dot(n, l), 1);
+    out_color  = mix(color, color + 0.5, edge);
 }
