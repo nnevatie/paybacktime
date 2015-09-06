aef9a41327a38b87e3c14c426ac06519f33087f4
Niko Nevatie <nnevatie@gmail.com>
2015-09-06 11:20:31 +0300

    Write normal output from Lambert shader.

--- shaders/lambert.fs.glsl
+++ shaders/lambert.fs.glsl
@@ -16,12 +16,14 @@ in Block
 ib;
 
 // Output
-out vec4 frag;
+out vec4 color;
+out vec4 normal;
 
 void main()
 {
     mat3 nm = transpose(inverse(mat3(mv)));
     vec3 n  = normalize(nm * ib.normal);
     vec3 c  = vec3(1, 1, 1) * (0.25 + max(dot(n, ldir), 0.0));
-    frag    = vec4(c, 1);
+    color   = vec4(c, 1);
+    normal  = vec4(n.x * 0.5 + 0.5, n.y * 0.5 + 0.5, n.z * 0.5 + 0.5, 1.0);
 }
