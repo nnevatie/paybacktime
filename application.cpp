4aa6e7372b1cce008baacf566cb938a78c4b9bca
Niko Nevatie <nnevatie@gmail.com>
2015-09-15 16:20:57 +0300

    Prepare for SSAO and blur rendering passes.

--- application.cpp
+++ application.cpp
@@ -67,12 +67,16 @@ bool Application::run()
     gl::Shader fsLambert(filesystem::path("shaders/lambert.fs.glsl"));
     gl::Shader fsScreenspace(filesystem::path("shaders/screenspace.fs.glsl"));
     gl::Shader fsTexture(filesystem::path("shaders/texture.fs.glsl"));
+    gl::Shader fsSsao(filesystem::path("shaders/ssao.fs.glsl"));
     gl::Shader gsWireframe(filesystem::path("shaders/wireframe.gs.glsl"));
 
     gl::ShaderProgram wireProgram({vsSimple, /*gsWireframe,*/ fsLambert},
                                  {{0, "position"}, {1, "normal"}, {2, "uv"}});
 
     gl::ShaderProgram blitProgram({vsSimple, fsTexture},
+                                 {{0, "position"}, {1, "normal"}, {2, "uv"}});
+
+    gl::ShaderProgram ssaoProgram({vsSimple, fsSsao},
                                  {{0, "position"}, {1, "normal"}, {2, "uv"}});
 
     const ImageCube meshSrc("data/floor.*.png", 1);
@@ -94,8 +98,8 @@ bool Application::run()
     {
         Clock clock;
         {
-            Binder<gl::Fbo> binder(ssao.fbo);
-
+            // Geometry pass
+            Binder<gl::Fbo> binder(ssao.fbo[0]);
             const GLenum drawBuffers[] = {GL_COLOR_ATTACHMENT0,
                                           GL_COLOR_ATTACHMENT1};
             glDrawBuffers(2, drawBuffers);
@@ -119,14 +123,28 @@ bool Application::run()
             primitive.render();
         }
 
-        glClearColor(0.f, 0.f, 0.f, 1.f);
-        glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
-        glEnable(GL_DEPTH_TEST);
+        ssaoProgram.bind().setUniform("mvp", glm::mat4());
+        {
+            // SSAO pass
+            Binder<gl::Fbo> binder(ssao.fbo[1]);
+            glDrawBuffer(GL_COLOR_ATTACHMENT0);
+            glClear(GL_COLOR_BUFFER_BIT);
+
+            ssao.texColor.bindAs(GL_TEXTURE0);
+            ssao.texNormal.bindAs(GL_TEXTURE1);
+            ssao.texDepth.bindAs(GL_TEXTURE2);
+            ssao.texNoise.bindAs(GL_TEXTURE3);
+            rectPrimitive.render();
+        }
 
         blitProgram.bind().setUniform("mvp", glm::mat4());
         {
-            Binder<gl::Texture> binder(ssao.texColor);
-            glActiveTexture(GL_TEXTURE0);
+            // Blur/output pass
+            glClearColor(0.f, 0.f, 0.f, 1.f);
+            glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
+            glEnable(GL_DEPTH_TEST);
+
+            ssao.texBlur.bindAs(GL_TEXTURE0);
             rectPrimitive.render();
         }
 
