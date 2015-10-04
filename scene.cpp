0b3a86a41ec7d965f13c1cdab7fec0bf5d7896c6
Niko Nevatie <nnevatie@gmail.com>
2015-10-04 19:43:32 +0300

    Add scene stub files.

---
+++ scene.cpp
@@ -0,0 +1,15 @@
+#include "scene.h"
+
+namespace hc
+{
+
+Scene::Scene()
+{
+}
+
+Scene& Scene::render()
+{
+    return *this;
+}
+
+} // namespace hc
---
+++ scene.h
@@ -0,0 +1,13 @@
+#pragma once
+
+namespace hc
+{
+
+struct Scene
+{
+    Scene();
+
+    Scene& render();
+};
+
+} // namespace hc
