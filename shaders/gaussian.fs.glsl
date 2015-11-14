177070193a13a8e239843b8a220378de2c16a524
Niko Nevatie <nnevatie@gmail.com>
2015-11-14 18:50:28 +0200

    Implement 11 px radius 1D gaussian filter.

--- shaders/gaussian.fs.glsl
+++ shaders/gaussian.fs.glsl
@@ -3,7 +3,9 @@
 // Uniforms
 uniform sampler2D tex;
 uniform bool      horizontal;
-uniform int       radius = 5;
+uniform float     weight[5] = float[]
+                  (0.227027, 0.1945946, 0.1216216, 0.054054, 0.016216);
+
 // Input
 in Block
 {
@@ -17,12 +19,13 @@ out vec4 color;
 
 void main()
 {
-    vec2 ts = 1.0 / textureSize(tex, 0);
-    vec2 d  = horizontal ? vec2(ts.x, 0) : vec2(0, ts.y);
-
-    vec3 result;
-    for(int i = -radius; i <= radius; ++i)
-        result += texture(tex, ib.uv + i * d).rgb;
-
-    color = vec4(result / (radius * 2 + 1), 1.0);
+    vec2 ts     = 1.0 / textureSize(tex, 0);
+    vec2 d      = horizontal ? vec2(ts.x, 0) : vec2(0, ts.y);
+    vec3 result = texture(tex, ib.uv).rgb * weight[0];
+    for(int i = 1; i < 5; ++i)
+    {
+        result += texture(tex, ib.uv - i * d).rgb * weight[i];
+        result += texture(tex, ib.uv + i * d).rgb * weight[i];
+    }
+    color = vec4(result, 1.0);
 }
