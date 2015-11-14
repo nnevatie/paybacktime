75293ed2f7d0d24b1f585868b6863fc46e3d955e
Niko Nevatie <nnevatie@gmail.com>
2015-11-14 18:50:51 +0200

    Remove emission related code from lighting stage.

--- shaders/lighting.fs.glsl
+++ shaders/lighting.fs.glsl
@@ -5,7 +5,6 @@ uniform sampler2D texDepth;
 uniform sampler2D texNormal;
 uniform sampler2D texColor;
 uniform sampler2D texLight;
-uniform sampler2D texEmissive;
 uniform sampler2D texAo;
 uniform sampler2D texGi;
 uniform mat4      v;
@@ -46,7 +45,6 @@ void main(void)
     vec3 normal     = texture(texNormal, ib.uv).rgb;
     vec3 albedo     = texture(texColor, ib.uv).rgb;
     vec3 light      = texture(texLight, ib.uv).rgb;
-    vec3 emission   = texture(texEmissive, ib.uv).rgb;
     vec3 ao         = texture(texAo, ib.uv).r *
                       textureBicubic(texGi, worldPos.xz).rgb;
     // Ambient
@@ -54,7 +52,7 @@ void main(void)
 
     // Diffuse
     vec3 viewDir    = normalize(fragPos);
-    vec3 lightPos   = vec3(10.0, 100.0, 1000.0);
+    vec3 lightPos   = vec3(10.0, 10.0, 1000.0);
     vec3 lightDir   = normalize(lightPos - fragPos);
     vec3 diffuse    = 1.0 * max(albedo * ao,
                                 albedo * max(dot(normal, lightDir), 0.0));
@@ -64,7 +62,6 @@ void main(void)
     float spec      = pow(max(dot(normal, halfwayDir), 0.0), 16.0);
     vec3 specular   = light.r * vec3(spec);
 
-    vec3 lighting   = ambient + ao * diffuse + ao * specular + emission;
+    vec3 lighting   = ambient + ao * diffuse + ao * specular;
     color           = vec4(lighting, 1.0);
-    //color           = vec4(emission, 1.0);
 }
