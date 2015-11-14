2ffd956cb5316736d1b637c3e62156c450f25935
Niko Nevatie <nnevatie@gmail.com>
2015-11-14 18:49:49 +0200

    Rename emissive shader to bloom.

--- shaders/bloom.fs.glsl
+++ shaders/bloom.fs.glsl
@@ -1,6 +1,7 @@
 #version 150
 
 // Uniforms
+uniform sampler2D texAlbedo;
 uniform sampler2D texColor;
 uniform sampler2D texLight;
 
@@ -17,5 +18,7 @@ out vec4 color;
 
 void main(void)
 {
-    color = texture(texColor, ib.uv) * texture(texLight, ib.uv).b;
+    float b = smoothstep(1.25, 3.0, length(texture(texColor, ib.uv)));
+    color   = b + texture(texAlbedo, ib.uv) *
+                  pow(texture(texLight, ib.uv).b * 2.0, 2.0);
 }
