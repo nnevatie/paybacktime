6e18cd321d83881baefea9b4918375d1f89c0d59
Niko Nevatie <nnevatie@gmail.com>
2015-11-14 18:49:30 +0200

    Use length function instead of dot.

--- shaders/denoise.fs.glsl
+++ shaders/denoise.fs.glsl
@@ -17,11 +17,12 @@ out vec4 color;
 void main(void)
 {
     vec2 ts  = 1.0 / vec2(textureSize(tex, 0));
+
     vec4 s   = texture(tex, ib.uv);
     vec4 avg = 0.25 * (texture(tex, vec2(ib.uv.s - ts.s, ib.uv.t))        +
                        texture(tex, vec2(ib.uv.s + ts.s, ib.uv.t))        +
                        texture(tex, vec2(ib.uv.s,        ib.uv.t - ts.t)) +
                        texture(tex, vec2(ib.uv.s,        ib.uv.t + ts.t)));
     vec4 d   = abs(s - avg);
-    color    = dot(d, d) > 0.5 ? avg : s;
+    color    = length(d) > 0.5 ? avg : s;
 }
