a482e72ce79572e8421231b1948e56b74c47362c
Niko Nevatie <nnevatie@gmail.com>
2015-12-04 17:45:56 +0200

    Clean up shader code.

--- shaders/geometry.fs.glsl
+++ shaders/geometry.fs.glsl
@@ -29,7 +29,9 @@ float edge(vec3 bc)
 void main()
 {
     normal = normalize(ib.normal);
-    color  = texture(texAlbedo, ib.uv);
     light  = texture(texLight, ib.uv).rgb;
+
+    vec4 alb = texture(texAlbedo, ib.uv);
+    color  = alb;
     //color = vec4(mix(alb + 0.25, alb, edge(ib.bc)).rgb, 1.0);
 }
