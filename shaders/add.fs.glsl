0b90230b7b26d05baaea4cc4a09f42d207f184e7
Niko Nevatie <nnevatie@gmail.com>
2015-12-13 16:55:50 +0200

    Add FXAA fragment shader. Add tonemapping to output shader.

--- shaders/add.fs.glsl
+++ shaders/add.fs.glsl
@@ -15,7 +15,23 @@ ib;
 // Output
 out vec4 color;
 
+float A = 0.15;
+float B = 0.50;
+float C = 0.10;
+float D = 0.20;
+float E = 0.02;
+float F = 0.30;
+float W = 11.2;
+
+vec3 tonemap(vec3 x)
+{
+   return ((x * (A * x + C * B) + D * E) / (x * (A * x + B) + D * F)) - E / F;
+}
+
 void main(void)
 {
-    color = texture(tex0, ib.uv) + texture(tex1, ib.uv);
+   float eb = 0.25f;
+   vec3 c0  = 20 * (texture(tex0, ib.uv) + texture(tex1, ib.uv)).rgb;
+   vec3 c1  = tonemap(eb * c0);
+   color    = vec4(c1 * vec3(1.0) / tonemap(vec3(W)), 1);
 }
