af68389385ede338ad6f94955c98eb2aa4544ee9
Niko Nevatie <nnevatie@gmail.com>
2016-01-11 22:35:09 +0200

    Add skysphere.

--- shaders/grid.fs.glsl
+++ shaders/grid.fs.glsl
@@ -31,9 +31,19 @@ vec4 grid(vec2 p, vec4 color)
            max(1.05 * sin((p.x + 4.0) * frq), 1.05 * sin((p.y + 4.0) * frq)));
 }
 
+vec4 sphere(vec3 p)
+{
+    float t = 0.5 - asin(p.y) / PI;
+    float a = smoothstep(0.1, 0.99, clamp(t + 0.2, 0, 1));
+    vec3  c = mix(vec3(0.0, 0.025, 0.05), vec3(0.0, 0.0, 0.0), a);
+    return vec4(c, 1);
+}
+
 void main(void)
 {
-    vec3  r = normalize(ib.viewRay) * mat3(v);
-    float d = intersect(pos, r);
-    color   = max(0, 1 - d * 0.0015) * grid((pos + d * r).xz, gridColor);
+    vec3  ray = normalize(ib.viewRay) * mat3(v);
+    float dg  = intersect(pos, ray);
+    vec3  ps  = normalize(ray);
+    color     = sphere(ps) +
+                max(0, 1 - dg * 0.0015) * grid((pos + dg * ray).xz, gridColor);
 }
