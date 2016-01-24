a9efbfd3e007c49b20a9ef2b882ce60e0c3c66b3
Niko Nevatie <nnevatie@gmail.com>
2016-01-24 22:54:08 +0200

    Clean up code slightly.

--- img/image_cube.cpp
+++ img/image_cube.cpp
@@ -99,14 +99,9 @@ ImageCube ImageCube::scaled(const ImageCube& refCube) const
         const Image& i1 = refCube.sides[i];
         if (i0.size() != i1.size())
         {
-            if (i0)
-                imageCube.sides[i] = i0.scaled(i1.size());
-            else
-            {
-                Image image(i1.size(), i1.depth());
-                image.fill(0x0);
-                imageCube.sides[i] = image;
-            }
+            imageCube.sides[i] = i0 ? i0.scaled(i1.size()) :
+                                      Image(i1.size(), i1.depth()).fill(0x0);
+
             HCLOG(Info) << "size mismatch on side " << i;
         }
     }
