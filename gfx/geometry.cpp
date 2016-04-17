d55de135719f14c620c6510d496550c3e06533d8
Niko Nevatie <nnevatie@gmail.com>
2016-04-17 19:21:33 +0300

    Add transparent geometry graphics pass.

--- gfx/geometry.cpp
+++ gfx/geometry.cpp
@@ -12,16 +12,22 @@ Geometry::Geometry(const Size<int>& renderSize) :
     rect(squareMesh()),
     vsQuad(gl::Shader::path("quad_uv.vs.glsl")),
     vsGeometry(gl::Shader::path("geometry.vs.glsl")),
+    vsGeometryTransparent(gl::Shader::path("geometry_transparent.vs.glsl")),
     gsWireframe(gl::Shader::path("wireframe.gs.glsl")),
     fsGeometry(gl::Shader::path("geometry.fs.glsl")),
+    fsGeometryTransparent(gl::Shader::path("geometry_transparent.fs.glsl")),
+    fsOitComposite(gl::Shader::path("oit_composite.fs.glsl")),
     fsDenoise(gl::Shader::path("denoise.fs.glsl")),
     fsCommon(gl::Shader::path("common.fs.glsl")),
     progGeometry({vsGeometry, gsWireframe, fsGeometry, fsCommon},
                 {{0, "position"}, {1, "normal"}, {2, "uv"}}),
+    progGeometryTransparent({vsGeometryTransparent, fsGeometryTransparent, fsCommon},
+                            {{0, "position"}, {1, "normal"}, {2, "uv"}}),
+    progOitComposite({vsQuad, fsOitComposite},
+                    {{0, "position"}, {1, "uv"}}),
     progDenoise({vsQuad, fsDenoise},
         {{0, "position"}, {1, "uv"}})
 {
-    // Texture and FBO
     auto fboSize = {renderSize.w, renderSize.h};
 
     texDepth.bind().alloc(fboSize,         GL_DEPTH_COMPONENT32F,
@@ -38,6 +44,15 @@ Geometry::Geometry(const Size<int>& renderSize) :
        .attach(texLight,         gl::Fbo::Attachment::Color, 2)
        .attach(texNormalDenoise, gl::Fbo::Attachment::Color, 3)
        .unbind();
+
+    // OIT
+    texOit0.bind().alloc(fboSize, GL_RGBA16F, GL_RGBA, GL_FLOAT);
+    texOit1.bind().alloc(fboSize, GL_R16F,    GL_RED,  GL_FLOAT);
+    fboOit.bind()
+          .attach(texDepth, gl::Fbo::Attachment::Depth)
+          .attach(texOit0,  gl::Fbo::Attachment::Color, 0)
+          .attach(texOit1,  gl::Fbo::Attachment::Color, 1)
+          .unbind();
 }
 
 Geometry& Geometry::operator()(
@@ -68,6 +83,7 @@ Geometry& Geometry::operator()(
     glViewport(0, 0, renderSize.w, renderSize.h);
     glClearColor(0.f, 0.f, 0.f, 1.f);
     glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
+
     texAlbedo->bindAs(GL_TEXTURE0);
     texLightmap->bindAs(GL_TEXTURE1);
 
@@ -91,9 +107,70 @@ Geometry& Geometry::operator()(
     gl::Fbo* fbo,
     gl::Texture* texAlbedo,
     gl::Texture* texLightmap,
+    gl::Texture* texGi,
+    gl::Texture* texIncid,
+    const Box& bounds,
     const Instances& instances,
-    const glm::mat4& v, const glm::mat4& p)
+    const Camera& camera)
 {
+    // OIT accumulation passes
+    {
+        Binder<gl::Fbo> binder(fboOit);
+        progGeometryTransparent.bind()
+                               .setUniform("texAlbedo",  0)
+                               .setUniform("texLight",   1)
+                               .setUniform("texGi",      2)
+                               .setUniform("texIncid",   3)
+                               .setUniform("boundsMin",  glm::floor(bounds.pos))
+                               .setUniform("boundsSize", glm::ceil(bounds.size))
+                               .setUniform("viewPos",    camera.position())
+                               .setUniform("v",          camera.matrixView())
+                               .setUniform("p",          camera.matrixProj());
+        glEnable(GL_DEPTH_TEST);
+        glDepthFunc(GL_LESS);
+        glDepthMask(false);
+        glEnable(GL_BLEND);
+
+        texAlbedo->bindAs(GL_TEXTURE0);
+        texLightmap->bindAs(GL_TEXTURE1);
+        texGi->bindAs(GL_TEXTURE2);
+        texIncid->bindAs(GL_TEXTURE3);
+
+        // Active buffer
+        const GLenum drawBuffers[] = {GL_COLOR_ATTACHMENT0,
+                                      GL_COLOR_ATTACHMENT1};
+        glDrawBuffers(2, drawBuffers);
+        glClearColor(0.f, 0.f, 0.f, 1.f);
+        glClear(GL_COLOR_BUFFER_BIT);
+
+        // Blend functions
+        glBlendFuncSeparate(GL_ONE,  GL_ONE,
+                            GL_ZERO, GL_ONE_MINUS_SRC_ALPHA);
+
+        // Render primitives
+        for (const auto& instance : instances)
+        {
+            progGeometryTransparent.setUniform("m", instance.second);
+            instance.first.render();
+        }
+        glDisable(GL_BLEND);
+    }
+    // OIT composition pass
+    {
+        Binder<gl::Fbo> binder(*fbo);
+        progOitComposite.bind()
+                        .setUniform("tex0", 0)
+                        .setUniform("tex1", 1);
+        glDrawBuffer(GL_COLOR_ATTACHMENT0);
+        glDisable(GL_DEPTH_TEST);
+        glDepthMask(false);
+        glEnable(GL_BLEND);
+        glBlendFunc(GL_ONE_MINUS_SRC_ALPHA, GL_SRC_ALPHA);
+        texOit0.bindAs(GL_TEXTURE0);
+        texOit1.bindAs(GL_TEXTURE1);
+        rect.render();
+        glDisable(GL_BLEND);
+    }
     return *this;
 }
 
--- gfx/geometry.h
+++ gfx/geometry.h
@@ -6,10 +6,14 @@
 #include <glm/mat4x4.hpp>
 
 #include "geom/size.h"
+#include "geom/box.h"
+
 #include "gl/primitive.h"
 #include "gl/shaders.h"
 #include "gl/texture.h"
 #include "gl/fbo.h"
+
+#include "scene/camera.h"
 
 namespace pt
 {
@@ -23,39 +27,51 @@ struct Geometry
 
     gl::Shader        vsQuad,
                       vsGeometry,
+                      vsGeometryTransparent,
                       gsWireframe,
                       fsGeometry,
+                      fsGeometryTransparent,
+                      fsOitComposite,
                       fsDenoise,
                       fsCommon;
 
     gl::ShaderProgram progGeometry,
+                      progGeometryTransparent,
+                      progOitComposite,
                       progDenoise;
 
     gl::Texture       texDepth,
                       texNormal,
                       texNormalDenoise,
                       texColor,
-                      texLight;
+                      texLight,
+                      texOit0,
+                      texOit1;
 
-    gl::Fbo           fbo;
+    gl::Fbo           fbo,
+                      fboOit;
 
     typedef std::pair<gl::Primitive, glm::mat4> Instance;
     typedef std::vector<Instance>               Instances;
 
     Geometry(const Size<int>& renderSize);
 
+    // Opaque
     Geometry& operator()(gl::Texture* texAlbedo,
                          gl::Texture* texLightmap,
                          const Instances& instances,
                          const glm::mat4& v,
                          const glm::mat4& p);
 
+    // Transparent
     Geometry& operator()(gl::Fbo* fbo,
                          gl::Texture* texAlbedo,
                          gl::Texture* texLightmap,
+                         gl::Texture* texGi,
+                         gl::Texture* texIncid,
+                         const Box& bounds,
                          const Instances& instances,
-                         const glm::mat4& v,
-                         const glm::mat4& p);
+                         const Camera& camera);
 };
 
 } // namespace gfx
