7ffe07a426c40474e89dc031bb2afa337e7fc6b6
Niko Nevatie <nnevatie@gmail.com>
2016-11-13 14:06:55 +0200

    Simplify sobel filter.

--- shaders/outline.fs.glsl
+++ shaders/outline.fs.glsl
@@ -16,21 +16,21 @@ ib;
 // Output
 out vec4 color;
 
-vec3 s(int dx, int dy, sampler2D t, vec2 uv)
+float s(int dx, int dy, sampler2D t, vec2 uv)
 {
     vec2 tsi = 1.0 / textureSize(t, 0);
-    return texture(outline, uv + vec2(dx * tsi.x, dy * tsi.y)).rgb;
+    return texture(outline, uv + vec2(dx * tsi.x, dy * tsi.y)).r;
 }
 
 float sobel(sampler2D t, vec2 uv)
 {
-    vec3 hc = s(-1, -1, t, uv) *  1.0 + s( 0, -1, t, uv) *  2.0 +
-              s( 1, -1, t, uv) *  1.0 + s(-1,  1, t, uv) * -1.0 +
-              s( 0,  1, t, uv) * -2.0 + s( 1,  1, t, uv) * -1.0;
-    vec3 vc = s(-1, -1, t, uv) *  1.0 + s(-1,  0, t, uv) *  2.0 +
-              s(-1,  1, t, uv) *  1.0 + s( 1, -1, t, uv) * -1.0 +
-              s( 1,  0, t, uv) * -2.0 + s( 1,  1, t, uv) * -1.0;
-    return pow(length(vc * vc + hc * hc), 0.5);
+    float hc = s(-1, -1, t, uv) *  1.0 + s( 0, -1, t, uv) *  2.0 +
+               s( 1, -1, t, uv) *  1.0 + s(-1,  1, t, uv) * -1.0 +
+               s( 0,  1, t, uv) * -2.0 + s( 1,  1, t, uv) * -1.0;
+    float vc = s(-1, -1, t, uv) *  1.0 + s(-1,  0, t, uv) *  2.0 +
+               s(-1,  1, t, uv) *  1.0 + s( 1, -1, t, uv) * -1.0 +
+               s( 1,  0, t, uv) * -2.0 + s( 1,  1, t, uv) * -1.0;
+    return pow(sqrt(vc * vc + hc * hc), 0.5);
 }
 
 void main(void)
