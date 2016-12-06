c89a05ff035941886ec2685826a95257ff824d11
Niko Nevatie <nnevatie@gmail.com>
2016-12-06 22:07:41 +0200

    Change Y dimension of cell::SIZE to 16.

--- constants.h
+++ constants.h
@@ -10,14 +10,14 @@ namespace c
 namespace cell
 {
     // X/Z form the ground plane, Y points World-up
-    const auto SIZE = glm::vec3(8.f, 32.f, 8.f);
+    const auto SIZE = glm::vec3(8.f, 16.f, 8.f);
     const auto GRID = glm::vec3(16.f, 0.f, 16.f);
 }
 
 namespace object
 {
     constexpr auto SCALE    = 1.f,
-                   EXPOSURE = 0.15f;
+                   EXPOSURE = 0.25f;
 }
 
 } // namespace c
