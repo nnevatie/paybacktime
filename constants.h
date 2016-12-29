cb6adabf77f59a1c76327004da285a2db4b49567
Niko Nevatie <nnevatie@gmail.com>
2016-12-29 14:23:03 +0200

    Clean up code slightly.

--- constants.h
+++ constants.h
@@ -10,8 +10,8 @@ namespace c
 namespace cell
 {
     // X/Z form the ground plane, Y points World-up
-    const auto SIZE = glm::vec3(8.f,  8.f, 8.f);
-    const auto GRID = glm::vec3(16.f, 0.f, 16.f);
+    const auto SIZE = glm::vec3(8.f,  8.f, 8.f),
+               GRID = glm::vec3(16.f, 0.f, 16.f);
 }
 
 namespace object
