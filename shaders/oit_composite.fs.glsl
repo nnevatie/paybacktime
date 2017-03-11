5c77d2f90ae304025ad566a6ced8512df109ab16
Niko Nevatie <nnevatie@gmail.com>
2017-03-11 13:13:44 +0200

    Clean up code.

--- shaders/oit_composite.fs.glsl
+++ shaders/oit_composite.fs.glsl
@@ -22,7 +22,7 @@ void main(void)
     vec4 accum  = texture(texOit0, ib.uv);
     float alpha = 1.0 - clamp(accum.a, 0.0, 1.0);
     accum.a     = texture(texOit1, ib.uv).r;
-    vec3 opaque = alpha > 0.0 ? textureLod(texEnv, ib.uv, alpha * 2).rgb :
+    vec3 opaque = alpha > 0.0 ? textureLod(texEnv, ib.uv, 2 * alpha).rgb :
                                 texture(texOpq, ib.uv).rgb;
     vec3 trans  = vec3(accum.rgb / clamp(accum.a, 1e-4, 5e4));
     color       = vec4(mix(opaque, trans, alpha), 1.0);
