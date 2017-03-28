30148eb208a261e4d8df22df3e8868c07e096a0d
Niko Nevatie <nnevatie@gmail.com>
2017-03-28 22:21:45 +0300

    Add basic skeleton creation place holder.

--- scene/character.cpp
+++ scene/character.cpp
@@ -1,18 +1,46 @@
 #include "character.h"
 
+#include <ozz/animation/offline/raw_skeleton.h>
+
 #include "common/log.h"
 #include "object.h"
 
 namespace pt
 {
+// Ozz types
+using ozz::math::Float3;
+using ozz::math::Quaternion;
+using ozz::animation::offline::RawSkeleton;
+
+namespace
+{
+
+void setupSkeleton(RawSkeleton& skeleton)
+{
+    {
+        // Root
+        skeleton.roots.resize(1);
+        auto& root = skeleton.roots.front();
+        root.name = "root";
+        auto& transform = root.transform;
+        transform.scale       = Float3::one();
+        transform.translation = Float3(0.f, 0.f, 0.f);
+        transform.rotation    = Quaternion::identity();
+
+        // TODO: read the skeleton data from character.json
+    }
+}
+
+} // namespace
 
 struct Character::Data
 {
-    static const int partCount = 15;
+    static constexpr int PART_COUNT = 15;
+    using Parts = std::array<Object, PART_COUNT>;
 
     Data(const fs::path& path, TextureStore& textureStore)
     {
-        const std::string partDirs[partCount] =
+        const std::string partDirs[PART_COUNT] =
         {
             "head",
             "torso",
@@ -30,22 +58,24 @@ struct Character::Data
             "hand.left",
             "hand.right"
         };
-        for (int i = 0; i < partCount; ++i)
+        for (int i = 0; i < PART_COUNT; ++i)
         {
             const fs::path partPath(path / partDirs[i]);
             if (fs::exists(partPath))
                 parts[i] = Object({partPath, path}, {}, textureStore);
         }
-        const int fallbacks[partCount] =
+        const int fallbacks[PART_COUNT] =
         {
             -1, -1, -1, 4, 3, 6, 5, 8, 7, 10, 9, 12, 11, 14, 13
         };
-        for (int i = 0; i < partCount; ++i)
-            if (!parts[i])
-                parts[i] = parts[fallbacks[i]].flipped(textureStore);
+        for (int i = 0; i < PART_COUNT; ++i)
+            if (!parts[i]) parts[i] = parts[fallbacks[i]].flipped(textureStore);
+
+        setupSkeleton(skeleton);
     }
 
-    std::array<Object, partCount> parts;
+    Parts       parts;
+    RawSkeleton skeleton;
 };
 
 Character::Character()
