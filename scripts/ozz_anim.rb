62673f650083c23855612710894dc3e1f3eee507
Niko Nevatie <nnevatie@gmail.com>
2017-04-18 19:36:45 +0300

    Add script for importing skeletons and animations.

---
+++ scripts/ozz_anim.rb
@@ -0,0 +1,15 @@
+# Create skeleton
+skeleton = "#{ARGV[1]}/skeleton.ozz"
+system("fbx2skel --file=#{ARGV[0]}/pose.fbx --skeleton=#{skeleton}")
+
+# Create animations archive
+archive = "#{ARGV[1]}/archive.ozz"
+File.open(archive, 'wb') do |output|
+  Dir.foreach(ARGV[0]) do |file|
+    next if file == '.' or file == '..'
+    animation = "#{ARGV[1]}/#{File.basename(file, ".fbx")}.ozz"
+    system("fbx2anim --file=#{ARGV[0]}/#{file} --skeleton=#{skeleton} " \
+           "--animation=#{animation}")
+    #output.write(File.open(animation, 'rb').read)
+  end
+end
