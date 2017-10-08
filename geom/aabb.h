684be9fe6152de9a2405ddc0ef2ea96e7594cd46
Niko Nevatie <nnevatie@gmail.com>
2017-10-08 20:04:35 +0300

    Use min/max in AABB.

--- geom/aabb.h
+++ geom/aabb.h
@@ -1,8 +1,11 @@
 #pragma once
 
+#include <array>
+
 #include <glm/vec3.hpp>
 #include <glm/common.hpp>
 #include <glm/gtx/component_wise.hpp>
+#include <glm/gtx/string_cast.hpp>
 
 #include "ray.h"
 #include "common/log.h"
@@ -12,52 +15,61 @@ namespace pt
 
 struct Aabb
 {
-    typedef glm::vec3 V;
+    using V = glm::vec3;
+    using Vertices = std::array<V, 8>;
 
     Aabb()
     {}
 
-    Aabb(const V& pos, const V& size) :
-        pos(pos), size(size)
+    Aabb(const V& min, const V& max) :
+        min(min), max(max)
     {}
 
-    inline V min() const
+    Aabb(const std::array<V, 8>& vertices) :
+        min(std::numeric_limits<float>::max()),
+        max(std::numeric_limits<float>::min())
     {
-        return pos;
+        for (const auto v : vertices)
+        {
+            min = glm::min(min, v);
+            max = glm::max(max, v);
+        }
+        #if 0
+        PTLOG(Info) << "min: " << glm::to_string(min) << ", "
+                    << "max: " << glm::to_string(max);
+        #endif
     }
 
-    inline V max() const
+    inline V size() const
     {
-        return pos + size;
+        return max - min;
     }
 
     inline V center() const
     {
-        return pos + 0.5f * size;
+        return 0.5f * (min + max);
     }
 
     inline explicit operator bool() const
     {
-        return pos != V() || size != V();
+        return min != V() || max != V();
     }
 
-    inline bool operator==(const Aabb& box) const
+    inline bool operator==(const Aabb& aabb) const
     {
-        return pos == box.pos && size == box.size;
+        return min == aabb.min && max == aabb.max;
     }
 
-    inline bool operator!=(const Aabb& box) const
+    inline bool operator!=(const Aabb& aabb) const
     {
-        return !operator==(box);
+        return !operator==(aabb);
     }
 
     inline Aabb operator|(const Aabb& aabb) const
     {
         if (!*this) return aabb;
         if (!aabb)  return *this;
-        auto posMin = glm::min(pos, aabb.pos);
-        auto posMax = glm::max(pos + size, aabb.pos + aabb.size);
-        return {posMin, posMax - posMin};
+        return {glm::min(min, aabb.min), glm::max(max, aabb.max)};
     }
 
     inline Aabb& operator|=(const Aabb& aabb)
@@ -66,28 +78,17 @@ struct Aabb
         return *this;
     }
 
-    inline Aabb rotated(int r)
+    inline Aabb rotated(int /*r*/)
     {
-        const auto c = center();
-        const auto s = 0.5f * size;
-        switch (r & 0x03)
-        {
-            case 0:
-            case 2:
-                return *this;
-            case 1:
-            case 3:
-                return Aabb(c - V(s.z, s.y, s.x), V(size.z, size.y, size.x));
-        }
-        return {};
+        return Aabb(vertices());
     }
 
     inline bool intersect(const Aabb& aabb) const
     {
-        const V a0 = min();
-        const V a1 = max();
-        const V b0 = aabb.min();
-        const V b1 = aabb.max();
+        const V a0 = min;
+        const V a1 = max;
+        const V b0 = aabb.min;
+        const V b1 = aabb.max;
         return !(b0.x >= a1.x || b1.x <= a0.x ||
                  b0.y >= a1.y || b1.y <= a0.y ||
                  b0.z >= a1.z || b1.z <= a0.z);
@@ -95,8 +96,8 @@ struct Aabb
 
     inline bool intersect(const Ray& ray) const
     {
-        V t0         = (min() - ray.pos) * ray.dirInv;
-        V t1         = (max() - ray.pos) * ray.dirInv;
+        V t0         = (min - ray.pos) * ray.dirInv;
+        V t1         = (max - ray.pos) * ray.dirInv;
         V tMin       = glm::min(t0, t1);
         V tMax       = glm::max(t0, t1);
         float cMin   = glm::compMax(tMin);
@@ -104,7 +105,22 @@ struct Aabb
         return cMax >= cMin;
     }
 
-    V pos, size;
+    inline Vertices vertices() const
+    {
+        return
+        {{
+            V(min.x, min.y, min.z),
+            V(min.x, min.y, max.z),
+            V(max.x, min.y, max.z),
+            V(max.x, min.y, min.z),
+            V(max.x, max.y, max.z),
+            V(max.x, max.y, min.z),
+            V(min.x, max.y, min.z),
+            V(min.x, max.y, max.z)
+        }};
+    }
+
+    V min, max;
 };
 
 } // namespace
