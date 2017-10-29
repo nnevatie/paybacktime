543dee942eb30b6ea3dafa504cacba212d5cc7ec
Niko Nevatie <nnevatie@gmail.com>
2017-10-29 12:27:26 +0200

    Improve preview lighting.

--- shaders/model.fs.glsl
+++ shaders/model.fs.glsl
@@ -16,14 +16,14 @@ out vec4 color;
 
 void main()
 {
-    vec4 albedo     = texture(texAlbedo, ib.uv);
+    vec3 albedo     = texture(texAlbedo, ib.uv).rgb;
 
     // Ambient
-    vec3 ambient    = vec3(0.1);
+    vec3 ambient    = 0.25 * albedo;
 
     vec3 lightDir   = normalize(vec3(-0.5, 1.0, -0.25));
-    vec3 diffuse    = 2.5 * albedo.rgb * max(dot(ib.normal, lightDir), 0.0);
+    vec3 diffuse    = 2.5 * albedo * max(dot(ib.normal, lightDir), 0.0);
 
-    vec3 c          = pow(max(ambient, diffuse), vec3(1.0 / 2.2));
+    vec3 c          = pow(ambient + diffuse, vec3(1.0 / 2.2));
     color           = vec4(c, dot(c.rgb, vec3(0.299, 0.587, 0.114)));
 }
